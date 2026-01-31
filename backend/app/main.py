c212df10cb3c316150a7a2542eb6f98ef92e5559
Matthew Noble <noblem@tcd.ie>
2026-01-31 14:49:13 +0000

    Added mock GET /users endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,3 +7,17 @@ app = FastAPI()
 def root():
     return {"message": "Rostering system backend running"}
 
+@app.get("/users")
+def get_users():
+    return [
+        {
+            "id": 1,
+            "name": "Alice",
+            "role": "Sales Associate"
+        },
+        {
+            "id": 2,
+            "name": "Bob",
+            "role": "Manager"
+        }
+    ]
