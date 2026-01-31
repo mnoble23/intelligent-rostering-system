b5fab2b7aef484bc07cdafd2d1e95d92dcff59cb
Matthew Noble <noblem@tcd.ie>
2026-01-31 16:53:56 +0000

    Added mock GET /roster endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -29,3 +29,25 @@ def get_availability():
         {"user_id": 2, "day": "Monday", "available_hours": ["08:00-16:00"]},
         {"user_id": 1, "day": "Tuesday", "available_hours": ["09:00-12:00"]}
     ]
+
+@app.get("/roster")
+def get_roster():
+    return [
+        {
+            "user_id": 1,
+            "week": "2026-02-03",
+            "shifts": [
+                {"day": "Monday", "hours": ["09:00-12:00", "14:00-18:00"]},
+                {"day": "Tuesday", "hours": ["09:00-12:00"]}
+            ]
+        },
+        {
+            "user_id": 2,
+            "week": "2026-02-03",
+            "shifts": [
+                {"day": "Monday", "hours": ["08:00-16:00"]},
+                {"day": "Wednesday", "hours": ["09:00-12:00", "13:00-17:00"]}
+            ]
+        }
+    ]
+
