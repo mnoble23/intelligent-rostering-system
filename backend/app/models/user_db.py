65a1ef6b0a8ccfac7699e13ebeef41523141325a
Matthew Noble <matthewnoble2002@gmail.com>
2026-02-10 21:28:59 +0000

    Implemented POST /users endpoint to add users to database and GET /users now returns real data

--- backend/app/models/user_db.py
+++ backend/app/models/user_db.py
@@ -4,5 +4,5 @@ from app.db.base import Base
 class UserDB(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String, nullable=False)
