9146958fd55fc105869533dc16c78e5a584db259
Matthew Noble <matthewnoble2002@gmail.com>
2026-02-10 20:57:48 +0000

    Added user and shift assignment tables

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,2 +1,4 @@
+from .user_db import UserDB
 from .availability_db import AvailabilityDB
 from .shift_db import ShiftDB
+from .shift_assignment_db import ShiftAssignmentDB
--- backend/app/models/shift_db.py
+++ backend/app/models/shift_db.py
@@ -5,7 +5,7 @@ class ShiftDB(Base):
     __tablename__ = "shift"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
-    day_of_week = Column(Integer, nullable=False)  # 0 = Monday, 6 = Sunday
+    day_of_week = Column(Integer, nullable=False) 
     start_time = Column(Time, nullable=False)
     end_time = Column(Time, nullable=False)
+
