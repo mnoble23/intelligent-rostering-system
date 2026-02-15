c016810b05fb4903a682d04c3c5c1da64292b6b7
Matthew Noble <matthewnoble2002@gmail.com>
2026-02-15 17:06:25 +0000

    Connected React app to FastAPI backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,10 +3,19 @@ from app.api import users, availability, roster
 from app.db.base import Base
 from app.db.session import engine
 import app.models
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(availability.router)
