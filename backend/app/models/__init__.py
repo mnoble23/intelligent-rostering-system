98c78d638ad999e33e06e84324de5829585fa9ad
Matthew Noble <matthewnoble2002@gmail.com>
2026-02-24 16:40:30 +0000

    Added workplace model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,3 +2,4 @@ from .user_db import UserDB
 from .availability_db import AvailabilityDB
 from .shift_db import ShiftDB
 from .shift_assignment_db import ShiftAssignmentDB
+from .workplace_db import WorkplaceDB
---
+++ backend/app/models/workplace_db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, DateTime, Integer, String
+from sqlalchemy.sql import func
+
+from app.db.base import Base
+
+
+class WorkplaceDB(Base):
+    __tablename__ = "workplace"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
